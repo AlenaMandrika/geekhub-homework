c28488c2a0a59edf201883af6a7e2d24d8a1e96d
alenamandrika <Alenenok0@ukr.net>
2017-10-10 23:35:22 +0300

    first homework. function

--- script2.js
+++ script2.js
@@ -50,6 +50,55 @@ console.log(minArr(arr_2,'min'));
 
 
 
+var arr_3 = [-2, -3, -4];
+function sum (arr, action) {
+    if (action === 'sum') {
+        var sum = 0;
+        for (var i = 0; i < arr.length; i++) {
+            if (typeof arr[i] === 'number' && !isNaN(arr[i])) {
+                sum += arr[i]
+            }
+        }
+        return sum;
+    }
+}
+console.log(sum(arr_3,'sum'));
+
+
+
+var arr_4 = [-2, -3, -4];
+function max (arr, action) {
+    if (action === 'max') {
+        var max = arr_4[0];
+        for (var i = 0; i < arr.length; i++) {
+            if (typeof arr[i] === 'number' && !isNaN(arr[i])) {
+                if (arr[i] > max) {
+                    max = arr[i]}
+            }
+        }
+        return max;
+    }
+}
+console.log(max(arr_4,'max'));
+
+
+
+var arr_5 = [-2, -3, -4];
+function min (arr, action) {
+    if (action === 'min') {
+        var min = 0;
+        for (var i = 0; i < arr.length; i++) {
+            if (typeof arr[i] === 'number' && !isNaN(arr[i])) {
+                if (arr[i] < min) {
+                    min = arr[i]}
+            }
+        }
+        return min;
+    }
+}
+console.log(min(arr_5,'min'));
+
+
 
 
 
